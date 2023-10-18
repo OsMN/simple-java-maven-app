6fbc7a79c31f53137b6cd2d0aa59b44607d59e9a
Francisco Jose Diaz Cano <franciscojosediazcano@emeal.nttdata.com>
2023-10-18 16:04:51 +0200

    Delete try catch

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,19 +10,14 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        try {
-            //int i = 0;
-            //while(true)
-            for(int i = 0; i<= 100 ; i++)
-            {
-                System.out.println(MESSAGE + " - v2- " + i);
-                Thread.sleep(5000);
-            //    i++;
-            }
-        }catch(Exception e) {
-			System.out.println(e);
-            throw e;
-		}
+        //int i = 0;
+        //while(true)
+        for(int i = 0; i<= 100 ; i++)
+        {
+            System.out.println(MESSAGE + " - v2- " + i);
+            Thread.sleep(5000);
+        //    i++;
+        }
     }
 
     public String getMessage() {
